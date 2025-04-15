3696ad50824c3194e2cab7a4e354dfc875417d94
8095128299 <ingalagianand030@gmail.com>
2025-04-15 09:54:14 +0530

    pushed

---
+++ CIE/operatorExample.java
@@ -0,0 +1,13 @@
+public class operatorExample {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 20;
+        System.out.println("Relational operators");
+        System.out.println("a == b: " + (a == b));
+        System.out.println("a >= b: " + (a >= b));
+        System.out.println("a <= b: " + (a <= b));
+        System.out.println("a != b: " + (a != b));
+        System.out.println("a > b: " + (a > b));
+        System.out.println("a < b: " + (a < b));
+    }
+}
